a579f532337d326bf6fff6db85d4913f3889ef96
AARON PAREDES CABRERA <iron0406@outlook.com>
2024-11-30 12:27:19 -0500

    chore: add last changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows access from remote IPs
+    port: 5173, // Your port
+    hmr: {
+      host: '18.206.133.13', // The public IP or domain
+      port: 5173,            // Match the port your Vite server is running on
+    },
+  },
 })
